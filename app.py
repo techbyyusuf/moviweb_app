e7e6e588387403711755554be6f517466ef4c31a
Saturooo3 <yusufbulukgiray@gmail.com>
2025-05-18 13:03:22 +0200

    add. errorhandler(500) and template

--- app.py
+++ app.py
@@ -116,8 +116,13 @@ def delete_movie(user_id,movie_id):
 
 
 @app.errorhandler(404)
-def page_not_found(e):
+def not_found_error(error):
     return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
 
 
 if __name__ == '__main__':
